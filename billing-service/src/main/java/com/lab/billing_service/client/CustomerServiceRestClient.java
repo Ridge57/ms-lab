b0e21520c990fc66ad956b4a768923ddaebaa743
Ridge57 <juniorridge93@yahoo.fr>
2026-03-29 14:04:24 +0200

    feat: monitoring prometheus + zipkin tracing

--- billing-service/src/main/java/com/lab/billing_service/client/CustomerServiceRestClient.java
+++ billing-service/src/main/java/com/lab/billing_service/client/CustomerServiceRestClient.java
@@ -1,6 +1,7 @@
 package com.lab.billing_service.client;
 
 import com.lab.billing_service.model.Customer;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -8,5 +9,15 @@ import org.springframework.web.bind.annotation.PathVariable;
 @FeignClient(name = "CUSTOMER-SERVICE")
 public interface CustomerServiceRestClient {
     @GetMapping(value = "/customers/{id}")
+    @CircuitBreaker(name = "customer-service", fallbackMethod = "getDefaultCustomer")
     Customer findCustomerById(@PathVariable Long id);
+
+    default Customer getDefaultCustomer(Long id, Exception e) {
+        return Customer
+                .builder()
+                .id(id)
+                .name("default name")
+                .email("default@email.com")
+                .build();
+    }
 }
